fbdd4e8090cc53c6a08abd7093fd8da03bcad182
Aman Nagar <amannagar3313@gmail.com>
2025-09-16 15:47:39 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Hero from './Hero.jsx';
 import Services from './Services.jsx';
-import About from './About.jsx';
+import About from './about.jsx';
 import PayPerCall from './PayPerCall.jsx';
 import Advertisers from './Advertisers.jsx';
 import Publishers from './Publishers.jsx';
